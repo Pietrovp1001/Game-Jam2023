fd9e136158995e4a2a82b90915b72fb9ec624241
QuantumBreak3678 <68030853+juandagongoradiaz@users.noreply.github.com>
2023-02-03 20:36:50 -0500

    Navegacion entre menu

--- GameJam2023/Assets/1. Main Project/Scripts/UI/MenuButtons.cs
+++ GameJam2023/Assets/1. Main Project/Scripts/UI/MenuButtons.cs
@@ -2,10 +2,34 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class MenuButtons : MonoBehaviour
 {
-    
+    public CanvasGroup SectionACanvas;
+    public CanvasGroup SectionBCanvas;
+    public GameObject SectionA;
+    public GameObject SectionB;
+
+    public void HideSection()
+    {
+        
+        SectionBCanvas.interactable = false; 
+        SectionBCanvas.alpha = 0f; 
+        SectionB.SetActive(true);
+        
+        StartCoroutine(Fadeout());
+    }
+    IEnumerator Fadeout()
+    {
+        SectionACanvas.DOFade(0f,0.5f).SetEase(Ease.InQuint);
+        SectionACanvas.interactable = false; 
+        yield return new WaitForSeconds(0.5f);
+        SectionBCanvas.interactable = true; 
+        SectionA.SetActive(false);
+        SectionBCanvas.DOFade(1f, 0.5f).SetEase(Ease.InQuint);
+    }
+
     public void ExitGame()
     {
         Debug.Log("Juego finalizado"); 
